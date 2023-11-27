54ee6b5e3b956ea020b8109daa2f7e94f72a96d3
joshua-mcfield99 <72096757+joshua-mcfield99@users.noreply.github.com>
2023-11-27 14:31:56 +0000

    Nav bar start

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -2,10 +2,13 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
+//Style
+import styles from '../styles/nav.module.css'
+
 export default function NavBar() {
   return (
-    <nav>
-        <div>
+    <nav className={styles.nav_container}>
+        <div className={styles.nav_logo}>
             <Image 
                 alt='Bear interior designs logo'
                 src='/BearLogoNav.png'
@@ -13,11 +16,14 @@ export default function NavBar() {
                 height={100}
             />
         </div>
-        <div>
+        <div className={styles.nav_links}>
             <Link href='/'>Home</Link>
             <Link href='/materials'>Materials</Link>
             <Link href='/gallery'>Gallery</Link>
             <Link href='/contact'>Contact</Link>
+        </div>
+        <div className={styles.nav_whatsapp}>
+
         </div>
     </nav>
   )
