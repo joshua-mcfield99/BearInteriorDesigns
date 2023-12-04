26af3c91c77d2e743c18d0497e616f082b824628
joshua-mcfield99 <72096757+joshua-mcfield99@users.noreply.github.com>
2023-12-04 11:42:57 +0000

    Hero section complete

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,22 @@ import styles from './page.module.css'
 
 export default function Home() {
     return (
-        <main>
-            Home
+        <main className={`${styles.main} main`}>
+            <div className={styles.hero}>
+                <div className={styles.inner}>
+                    <div className={styles.left}>
+                        <h1>Crafting Your Dream Living Spaces, One Detail At A Time</h1>
+                    </div>
+                    <div className={styles.right}>
+                        <p>
+                            Bespoke Bedroom Storage Soloutions in surrey and surrounding areas.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.about}>
+                
+            </div>
         </main>
     )
 }
