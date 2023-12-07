647b160ae9a83240f3967919d381757fb40e46dc
joshua-mcfield99 <72096757+joshua-mcfield99@users.noreply.github.com>
2023-12-07 15:39:56 +0000

    Nav almost done, only whatsapp button left

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,30 +1,49 @@
+'use client'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
 //Style
 import styles from '../styles/nav.module.css'
 
 export default function NavBar() {
-  return (
-    <nav className={styles.nav_container}>
-        <div className={styles.nav_logo}>
-            <Image 
-                alt='Bear interior designs logo'
-                src='/BearLogoNav.png'
-                width={187}
-                height={100}
-            />
-        </div>
-        <div className={styles.nav_links}>
-            <Link href='/'>Home</Link>
-            <Link href='/materials'>Materials</Link>
-            <Link href='/gallery'>Gallery</Link>
-            <Link href='/contact'>Contact</Link>
-        </div>
-        <div className={styles.nav_whatsapp}>
-            <Link href='/whatsapp' className={`${styles.whatsapp_link}`}>WhatsApp</Link>
-        </div>
-    </nav>
-  )
+    // State to track the menu's open/closed state
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    // Toggle function to change the state
+    const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    };
+
+    return (
+        <nav className={styles.nav_container}>
+            <div className={styles.nav_logo}>
+                <Image 
+                    alt='Bear interior designs logo'
+                    src='/BearLogoNav.png'
+                    width={187}
+                    height={100}
+                />
+            </div>
+            <div className={`
+                    ${styles.nav_controls} 
+                    ${ isMenuOpen ? `${styles.menu_open}` : ''}
+                `} 
+                onClick={toggleMenu}
+            >
+                <span className={`${styles.hamburger}`}></span>
+            </div>
+            <div className={`${styles.nav_links_container} ${ isMenuOpen ? `${styles.show_menu}` : ''}`}>
+                <div className={styles.nav_links}>
+                    <Link href='/' onClick={toggleMenu}>Home</Link>
+                    <Link href='/materials' onClick={toggleMenu}>Materials</Link>
+                    <Link href='/gallery' onClick={toggleMenu}>Gallery</Link>
+                    <Link href='/contact' onClick={toggleMenu}>Contact</Link>
+                </div>
+                <div className={styles.nav_whatsapp}>
+                    <Link href='/whatsapp' className={`${styles.whatsapp_link}`} onClick={toggleMenu}>WhatsApp</Link>
+                </div>
+            </div>
+        </nav>
+    )
 }
