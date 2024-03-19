f776d019bccaf8f517538a313d49950d1d36359a
joshua-mcfield99 <72096757+joshua-mcfield99@users.noreply.github.com>
2024-03-19 14:58:05 +0000

    Gallery Image fetch and minor styling

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -12,27 +12,31 @@ export default async function Gallery() {
     const images = data.images;
     console.log('Images', data.images);
   return (
-    <div className={`${styles.g_container}`}>
-        {images === undefined || images.length === 0 ? <h1>No Images Available</h1> : <h1>Gallery</h1>}
-            {images.map((image, idx) => {
-                return (
-                    <div key={idx} className={styles.image}>
-                        <CldImage 
-                            src={image.url}
-                            width='200'
-                            height='300'
-                            strictTransformations
-                            transformations={["fit-transformation"]}
-                        />
-                    </div>
-                )
-            })}
-    </div>
+    <main>
+        <div className={`${styles.g_container}`}>
+            <div className={`title`}>
+                {images === undefined || images.length === 0 ? <h1>No Images Available</h1> : <h1>Gallery</h1>}
+            </div>
+            <div className={`${styles.gallery}`}>
+                {images.map((image, idx) => {
+                    return (
+                        <div key={idx} className={styles.image}>
+                            <CldImage
+                                src={image.url}
+                                fill
+                                sizes="(max-width: 1440px) 100vw"
+                            />
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    </main>
   )
 }
 
 export async function getData() {
-    const res = await fetch(`https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/resources/image`, {
+    const res = await fetch(`https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/resources/image/upload?prefix=Gallery`, {
         headers: {
             Authorization:`Basic ${Buffer.from(process.env.API_KEY + ':' + process.env.API_SECRET).toString('base64')}`
         }
