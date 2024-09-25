741095ccca507fa918cf8f26dc409be0995c92dd
joshua-mcfield99 <72096757+joshua-mcfield99@users.noreply.github.com>
2024-09-25 12:52:52 +0100

    remove max-width from main

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -18,31 +18,33 @@ export default function NavBar() {
 
     return (
         <nav className={styles.nav_container}>
-            <div className={styles.nav_logo}>
-                <CldImage
-                    alt='Bear interior designs logo'
-                    src='Other/LogoNav'
-                    width={150}//134 original
-                    height={100}
-                />
-            </div>
-            <div className={`
-                    ${styles.nav_controls} 
-                    ${ isMenuOpen ? `${styles.menu_open}` : ''}
-                `} 
-                onClick={toggleMenu}
-            >
-                <span className={`${styles.hamburger}`}></span>
-            </div>
-            <div className={`${styles.nav_links_container} ${ isMenuOpen ? `${styles.show_menu}` : ''}`}>
-                <div className={styles.nav_links}>
-                    <Link href='/' onClick={toggleMenu}>Home</Link>
-                    <Link href='/materials' onClick={toggleMenu}>Materials</Link>
-                    <Link href='/gallery' onClick={toggleMenu}>Gallery</Link>
-                    <Link href='/contact' onClick={toggleMenu}>Contact</Link>
+            <div className={styles.nav_inner_container}>
+                <div className={styles.nav_logo}>
+                    <CldImage
+                        alt='Bear interior designs logo'
+                        src='Other/LogoNav'
+                        width={150}//134 original
+                        height={100}
+                    />
                 </div>
-                <div className={styles.nav_whatsapp}>
-                    <a href='https://wa.me/447955577947' target="_blank" rel="noopener noreferrer" className={`${styles.whatsapp_link}`} onClick={toggleMenu}>WhatsApp</a>
+                <div className={`
+                        ${styles.nav_controls} 
+                        ${ isMenuOpen ? `${styles.menu_open}` : ''}
+                    `} 
+                    onClick={toggleMenu}
+                >
+                    <span className={`${styles.hamburger}`}></span>
+                </div>
+                <div className={`${styles.nav_links_container} ${ isMenuOpen ? `${styles.show_menu}` : ''}`}>
+                    <div className={styles.nav_links}>
+                        <Link href='/' onClick={toggleMenu}>Home</Link>
+                        <Link href='/materials' onClick={toggleMenu}>Materials</Link>
+                        <Link href='/gallery' onClick={toggleMenu}>Gallery</Link>
+                        <Link href='/contact' onClick={toggleMenu}>Contact</Link>
+                    </div>
+                    <div className={styles.nav_whatsapp}>
+                        <a href='https://wa.me/447955577947' target="_blank" rel="noopener noreferrer" className={`${styles.whatsapp_link}`} onClick={toggleMenu}>WhatsApp</a>
+                    </div>
                 </div>
             </div>
         </nav>
